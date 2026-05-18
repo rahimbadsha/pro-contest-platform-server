881a065db888364b1357c7015beadf52c1f3dcf4
Rahim <rahimbadsha.cse@gmail.com>
2026-05-18 09:02:54 +0600

    Await DB connection per request to fix cold start timeout

--- src/app.js
+++ src/app.js
@@ -6,14 +6,21 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-connectDB().catch(console.error);
-
 app.use(cors({
   origin: [process.env.FRONTEND_URL || 'http://localhost:5173', process.env.CLIENT_URL || 'http://localhost:5173'],
   credentials: true,
 }));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    res.status(503).json({ message: 'Database unavailable' });
+  }
+});
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
